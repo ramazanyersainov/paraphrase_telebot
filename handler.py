6eb491d49af919274055313901d61401f347315a
ramazanyersainov <ramazan.yersainov@nu.edu.kz>
2019-12-02 18:19:41 +0600

    first commit

--- handler.py
+++ handler.py
@@ -2,13 +2,18 @@ import telebot
 from googletrans import Translator
 
 translator = Translator()
-bot = telebot.TeleBot('1001004694:AAHaLKSS0oz86QYnQqtuWm02aNcqY03m6Gc')
+f = open('token.txt')
+
+bot = telebot.TeleBot(f.readline().split('\n')[0])
+
+f.close()
 
 @bot.message_handler(content_types=['text'])
 def send_text(message):
 
     language = translator.detect(message.text).lang
     temp = translator.translate(message.text, dest='fr').text
+    temp = translator.translate(message.text, dest='mt').text
     temp = translator.translate(temp, dest=language).text
     bot.send_message(message.chat.id, temp)
 
